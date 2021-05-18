ce1ec92713d29b38c85885317dc3c496b5cc0676
xiaotiandada <istianlei@qq.com>
2021-05-19 02:41:18 +0800

    fix: eslint error

--- client/src/views/Custom/index.tsx
+++ client/src/views/Custom/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { ptwxz } from '../../api/index'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import Footer from '../../components/Footer'
