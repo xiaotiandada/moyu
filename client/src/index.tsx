7a4298c00d00eff06b1009844e62cb8c2b2b22b0
xiaotiandada <istianlei@qq.com>
2021-05-18 18:46:54 +0800

    feat: add pwa

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,7 @@ import { persistStore } from 'redux-persist'
 import './i18n';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import ThemeProvider, { FixedGlobalStyle, ThemedGlobalStyle } from './theme'
 import { store } from './store/index';
@@ -27,6 +28,11 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
