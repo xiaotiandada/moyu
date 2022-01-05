dba4b80de5148499362565f234db794cd168345f
xiaotiandada <istianlei@qq.com>
2022-01-05 12:46:02 +0800

    fix: :ambulance: fix router name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-import 'antd/dist/antd.css'
 import Layout from '../components/Layout'
+import 'antd/dist/antd.css'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
